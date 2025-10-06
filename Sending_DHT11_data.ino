cfd924d0e029750a6bb48ce1a06c7e4e95089102
Behdad Mohammadi <81512183+BehdadMohammadi@users.noreply.github.com>
2025-10-05 19:00:06 -0700

    Update Sending_DHT11_data.ino

--- Sending_DHT11_data.ino
+++ Sending_DHT11_data.ino
@@ -13,7 +13,7 @@ const char* ssid = "BEHDAD";
 const char* password = "behdad1234";
 
 // Your ThingsBoard access token
-const char* accessToken = "PfaLAKE9bTmmjx5srEfq";
+const char* accessToken = "****";
 
 // ThingsBoard server
 const char* server = "http://thingsboard.cloud";
